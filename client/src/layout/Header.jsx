69cb288822194a047ab22cd161c1585fc7fef59f
xanimnuriyev1 <xanimnuriyeva07@gmail.com>
2024-08-17 20:10:07 +0400

    header new commit

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { NavLink } from 'react-router-dom'
 import './header.css'
 import StarLogo from "../assets/star.png"
+import { FiAlignRight } from "react-icons/fi";
+import { IoClose } from "react-icons/io5";
 
 const Header = () => {
+const navRef=useRef()
+const showNav=()=>{
+    navRef.current.classList.toggle("responsive_nav")
+}
+
   return (
    <header className='navbar'>
    <div>
     <img src={StarLogo} className='StarLogo' alt="" />
    </div>
    <div>
-    <nav>
+    <nav ref={navRef}>
         <ul className='pages'>
             <li><NavLink to={'/'} className={(navData)=>navData.isActive ? "active" :""}>Home</NavLink></li>
             <li><NavLink to={'/about'} className={(navData)=>navData.isActive ? "active" :""}>About Me</NavLink></li>
             <li><NavLink to={'/services'} className={(navData)=>navData.isActive ? "active" :""}>Services</NavLink></li>
             <li><NavLink to={'/portfolio'} className={(navData)=>navData.isActive ? "active" :""}>Portfolio</NavLink></li>
             <li><NavLink to={'/contact'} className={(navData)=>navData.isActive ? "active" :""}>Contact</NavLink></li>
+            <li>
+                <button className='nav-btn' onClick={showNav}>
+                <IoClose />
+                </button>
+            </li>
         </ul>
     </nav>
    </div>
    <div>
     <button className='contactBtn'>contact with me</button>
+   </div>
+   <div>
+    <button className='nav-btn nav-close-btn' onClick={showNav}>
+    <FiAlignRight />
+    </button>
    </div>
    </header>
   )
