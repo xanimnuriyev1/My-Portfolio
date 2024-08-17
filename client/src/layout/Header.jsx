482c6e009ac49b0836e7344fde3480ac8b4709f3
xanimnuriyev1 <xanimnuriyeva07@gmail.com>
2024-08-17 19:21:42 +0400

    header new commit

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './header.css'
+import StarLogo from "../assets/star.png"
 
 const Header = () => {
   return (
-   <header>
-   <div>Alex</div>
+   <header className='navbar'>
+   <div>
+    <img src={StarLogo} className='StarLogo' alt="" />
+   </div>
    <div>
     <nav>
-        <ul>
+        <ul className='pages'>
             <li><NavLink to={'/'} className={(navData)=>navData.isActive ? "active" :""}>Home</NavLink></li>
             <li><NavLink to={'/about'} className={(navData)=>navData.isActive ? "active" :""}>About Me</NavLink></li>
             <li><NavLink to={'/services'} className={(navData)=>navData.isActive ? "active" :""}>Services</NavLink></li>
@@ -18,7 +21,7 @@ const Header = () => {
     </nav>
    </div>
    <div>
-    <button>contact with me</button>
+    <button className='contactBtn'>contact with me</button>
    </div>
    </header>
   )
