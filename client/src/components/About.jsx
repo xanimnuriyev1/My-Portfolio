ab79f180ba26c62e3055f9238cab64bc9b4cfbe1
xanimnuriyev1 <xanimnuriyeva07@gmail.com>
2024-08-18 23:19:57 +0400

    about new modified

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import "./pages.css"
+import Img from "../assets/Me2.jpg"
 
 const About = () => {
   return (
-    <div>
-      about
+    <section className='about-section'>
+      <div>
+        <img className='img' src={Img} alt="" />
+      </div>
+      <div>
+     <h1 className='about-head'>About Me</h1>
+     {/* <p className='About-text'>I am 21 year old. I am a young programming developer enthusiast. <br /> Therefore ,I am constantly learning and developing software.</p> */}
+     <div className="about-skills">
+      <div className="about-skill">
+        <p>HTML & CSS</p>
+        <hr style={{width:"50%"}}/>
+      </div>
+      <div className="about-skill">
+        <p>React JS</p>
+        <hr style={{width:"70%"}}/>
+      </div>
+      <div className="about-skill">
+        <p>Javascript</p>
+        <hr style={{width:"60%"}}/>
+      </div>
+      <div className="about-skill">
+        <p>Next Js</p>
+        <hr style={{width:"50%"}}/>
+      </div>
+      <div className="about-skill">
+        <p>Mongo DB</p>
+        <hr style={{width:"65%"}}/>
+      </div>
+      <div className="about-skill">
+        <p>Context</p>
+        <hr style={{width:"60%"}}/>
+      </div>
+      <div className="about-skill">
+        <p>Frontend librarires: Bootstrap,Ant-design,Material UI,tailwind CSS</p>
+        <hr style={{width:"70%"}}/>
+      </div>
+     </div>
     </div>
+    </section>
   )
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -12,19 +12,19 @@ const Home = () => {
     </div>
     <div>
      <h1>Hi, I am <span>Khanim Nuriyeva</span></h1>
+     <p className='Paraghraph'>Frontend developer based in Baku</p>
      <div className="icon-box">
      <div className="btn-group">
-      <a href=""><TbBrandGithubFilled /></a>
+      <a href="https://github.com/xanimnuriyev1"><TbBrandGithubFilled /></a>
      </div>
      <div className="btn-group">
-      <a href=""><FaLinkedinIn /></a>
+      <a href="https://www.linkedin.com/in/khanim-nuriyeva-5031652b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app "><FaLinkedinIn /></a>
      </div>
      <div className="btn-group">
-      <a href=""><FaFacebookF /></a>
+      <a href="https://www.facebook.com/people/Xan%C4%B1m-%C4%B0lqarl%C4%B1/pfbid0HNLWSuZRCVyT4LNn4kfHNBwP6FknafemqoXbduKhQHTv8Rn2cX2xHYWyTWGhz5Vkl/?mibextid=LQQJ4d"><FaFacebookF /></a>
      </div>
      </div>
     </div>
-   
     </div>
   )
 }
